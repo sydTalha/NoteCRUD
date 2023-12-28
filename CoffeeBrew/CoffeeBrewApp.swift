93cf0ef957183963fdcae4a97f6059172f372319
Syed Talha Rizwan <talharizwan013@gmail.com>
2023-12-28 15:07:33 +0500

    Initial Commit

---
+++ CoffeeBrew/CoffeeBrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBrewApp.swift
+//  CoffeeBrew
+//
+//  Created by Talha on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
